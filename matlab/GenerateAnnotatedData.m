ca9c67c3a4bacbea9096841440ac3bbe8f56fdcd
xiaofeng <xiaofeng419@gmail.com>
2018-11-09 00:14:55 -0800

    v7

--- matlab/GenerateAnnotatedData.m
+++ matlab/GenerateAnnotatedData.m
@@ -4,12 +4,12 @@
 %Mail: liuyeung@stanford.edu   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-vbbRoot = '/Users/yangliu/Desktop/courses/stanford/AI/final_project/data/JAAD_pedestrian/vbb_part';
-seqRoot = '/Users/yangliu/Desktop/courses/stanford/AI/final_project/data/JAAD_DATA/JAAD_videos';
-sampleRoot = '/Users/yangliu/Desktop/courses/stanford/AI/final_project/mydata';
-behaviorXMLPath = '/Users/yangliu/Desktop/courses/stanford/AI/final_project/data/JAAD_behavior/behavioral_data_xml';
+vbbRoot = '../../Dataset/JAAD_pedestrian/vbb_part';
+seqRoot = '../../Dataset/JAAD_videos';
+sampleRoot = '../../Dataset/Data_by_Matlab';
+behaviorXMLPath = '../../Dataset/JAAD_behavior/behavioral_data_xml';
 
 attAnn = ParseJAADBehAnnote('./pedestrian_behavior_attributes.txt');
 behAnn = ParseJAADBehXML(behaviorXMLPath);
-SampleCrossData(attAnn, behAnn, vbbRoot, seqRoot, sampleRoot);
-SampleNonCrossData(attAnn, behAnn, vbbRoot, seqRoot, sampleRoot);
+%SampleCrossData(attAnn, behAnn, vbbRoot, seqRoot, sampleRoot);
+SampleNonCrossData(attAnn, behAnn, vbbRoot,j seqRoot, sampleRoot);
